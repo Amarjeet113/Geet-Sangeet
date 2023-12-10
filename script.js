06668dc12bc54ef1cdebd4b2212d707c87ba47c7
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2023-12-10 19:41:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+console.log("Welcome to Sangeet");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Arijit Singh Collection", filePath: "songs/1.mp3", coverPath: "covers/1.jpeg"},
+    {songName: "Bhakti Songs Collection", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Bhojpuri Collection", filePath: "songs/3.mp3", coverPath: "covers/3.webp"},
+    {songName: "English Songs Collection", filePath: "songs/4.mp3", coverPath: "covers/4.jpeg"},
+    {songName: "Haryanvi Collection", filePath: "songs/5.mp3", coverPath: "covers/5.jpeg"},
+    {songName: "Punjabi Collection", filePath: "songs/6.mp3", coverPath: "covers/6.jpeg"},
+    {songName: "Marathi Collection", filePath: "songs/7.mp3", coverPath: "covers/7.jpeg"},
+    {songName: "Friendship Collection", filePath: "songs/8.mp3", coverPath: "covers/8.jpeg"},
+    {songName: "Romantic Collection", filePath: "songs/9.mp3", coverPath: "covers/9.jpeg"},
+    {songName: "Maithli Collection", filePath: "songs/10.mp3", coverPath: "covers/10.jpeg"},
+    {songName: "Mashup songs", filePath: "songs/11.mp3", coverPath: "covers/11.jpeg"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=10){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
